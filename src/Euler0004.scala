e5c86c8863a76900d5c57c4d2d880ab040493c16
Greg Merrill <greghmerrill@gmail.com>
2013-02-24 14:37:41 -0600

    add

---
+++ src/Euler0004.scala
@@ -0,0 +1,11 @@
+object Euler0004 extends EulerSolution {
+
+  override def expect = 906609
+
+  def solve = {
+    def isPalindrome(x: Int) = x.toString.equals(x.toString.reverse)
+    val combos = (100 to 999) map { x => (x to 999) map { y => x * y } } flatten;
+    combos filter (x => isPalindrome(x)) max
+  }
+
+}
