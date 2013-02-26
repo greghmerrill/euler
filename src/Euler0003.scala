16450ef06212504a83cb10a48f401d5e9f7f71b4
Greg Merrill <greghmerrill@gmail.com>
2013-02-25 20:30:28 -0600

    use prime factorization approach

--- src/Euler0003.scala
+++ src/Euler0003.scala
@@ -2,28 +2,13 @@ object Euler0003 extends EulerSolution {
 
   override def expect = 6857
 
-  def isPrime(n: Long): Boolean = n match {
-    case 2 => true
-    case n if n % 2 == 0 => false
-    case _ => {
-      var i = 3
-      while (i <= n / 2) {
-        if (n % i == 0) return false
-        i += 2
-      }
-      true
-    }
-  }
-
   def solve: Long = {
-    val target = 600851475143L
-    var i = 2
-    while (true) {
-      if (target % i == 0 && isPrime((target / i).toLong))
-        return (target / i).toLong
-      i += 1
-    }
-    throw new Exception("No prime factor for " + target)
+    def largestPrimeFactor(remaining:Long, factor:Long): Long = remaining match {
+      case 1 => factor
+      case n if (n % factor == 0) => largestPrimeFactor(n / factor, factor)
+      case _ => largestPrimeFactor(remaining, factor + 1)
+    } 
+    largestPrimeFactor(600851475143L, 2)
   }
 
 }
