81d3360bfcdbfa6288ec414d065057b20fd4ee59
Greg Merrill <greghmerrill@gmail.com>
2013-02-27 22:24:55 -0600

    Solution for Problem 7

---
+++ src/Euler0007.scala
@@ -0,0 +1,14 @@
+object Euler0007 extends EulerSolution {
+
+  override def expect = 104743
+
+  def solve = {
+    def nthPrime(nextVal: Int, primes: List[Int], n: Int): Int = {
+      if (primes.length == n) primes.head
+      else if (primes.reverse.exists(nextVal % _ == 0)) nthPrime(nextVal + 2, primes, n)
+      else nthPrime(nextVal + 2, nextVal :: primes, n)
+    }
+    nthPrime(3, List(2), 10001)
+  }
+
+}
