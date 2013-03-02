6b60373d369bfa365e3e33eb474eb5592a97c64d
Greg Merrill <greghmerrill@gmail.com>
2013-03-02 16:07:11 -0600

    solution 10

---
+++ src/Euler0010.scala
@@ -0,0 +1,37 @@
+import java.util.BitSet
+import scala.math._
+
+object Euler0010 extends EulerSolution {
+
+  override def expect = 142913828922L
+  
+  def solve = {
+    val below = 2000000
+    
+    val nonPrimes = new BitSet()
+    nonPrimes.set(0)
+    nonPrimes.set(1)
+    
+    var primes = List[Int]()
+    var prime = nonPrimes.nextClearBit(2)
+    
+    while (prime < sqrt(below).toInt + 1) {
+      primes = prime :: primes
+      var nonPrime = prime * 2
+      while (nonPrime < below) {
+        nonPrimes.set(nonPrime)
+        nonPrime += prime
+      }
+      prime = nonPrimes.nextClearBit(prime + 1)
+    }
+    
+    while (prime < below) {
+      primes = prime :: primes
+      prime = nonPrimes.nextClearBit(prime + 1)
+    }
+
+    primes.foldLeft(0L){(sum, next) => sum + next}
+  }
+
+}
+
