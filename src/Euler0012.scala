8b1e917e99bca6905691945d817c2e252014cd98
Greg Merrill <greghmerrill@gmail.com>
2013-03-10 17:01:23 -0500

    calculate using prime factors

--- src/Euler0012.scala
+++ src/Euler0012.scala
@@ -2,21 +2,24 @@ import scala.math._
 
 object Euler0012 extends EulerSolution {
 
+  override def expect = 76576500
+  
   def triangle(n: Int) = ((n * n) + n) / 2
 
-  def calcFactors(value: Int, n: Int, factors: List[Int]): List[Int] = {
-    n match {
-      case n if n > floor(sqrt(value)).toInt => factors
-      case n if value % n == 0 => calcFactors(value, n + 1, n :: value / n :: factors)
-      case _ => calcFactors(value, n + 1, factors)
-    }
+  def calcPrimeFactors(remaining: Int, curFactor: Int, factors: List[Int]): List[Int] = remaining match {
+    case 1 => factors
+    case n if n % curFactor == 0 => calcPrimeFactors(remaining / curFactor, curFactor, curFactor :: factors)
+    case _ => calcPrimeFactors(remaining, curFactor + 1, factors)
   }
-  def factors(value: Int) = calcFactors(value, 1, List())
+  def primeFactors(num: Int) = calcPrimeFactors(num, 2, List[Int]())
+
+  // # divisors for n = product of (count of each prime factor + 1)
+  def factors(n: Int):Int = primeFactors(n).groupBy(n => n).map(keyVal => keyVal._2.length + 1).product
 
   def solve = {
     def find(i: Int): Int = {
       val tri = triangle(i)
-      if (factors(tri).length > 500) tri else find(i + 1)
+      if (factors(tri) > 500) tri else find(i + 1)
     }
     find(2)
   }
