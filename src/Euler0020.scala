0ad4c3faae47253790a3e2eb31140b4cd7a683fc
Greg Merrill <greghmerrill@gmail.com>
2013-03-10 21:37:57 -0500

    solution 20

---
+++ src/Euler0020.scala
@@ -0,0 +1,12 @@
+object Euler0020 extends EulerSolution {
+
+  override def expect = 648
+
+  def factorial(n: BigInt) = {
+    def _factorial(n: BigInt, accum: BigInt): BigInt = if (n == 1) accum else _factorial(n - 1, accum * n)
+    _factorial(n, 1)
+  }
+
+  def solve = factorial(100).toString.map(c => ("" + c).toInt).sum
+
+}
