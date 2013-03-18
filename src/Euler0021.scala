19e267d64db9d62a3647a0aae68ace704caead98
Greg Merrill <greghmerrill@gmail.com>
2013-03-17 21:33:38 -0500

    Solutions 21, 22

---
+++ src/Euler0021.scala
@@ -0,0 +1,17 @@
+import scala.math._
+
+object Euler0021 extends EulerSolution {
+
+  override def expect = 31626
+  
+  def divisors(n: Int) = (1 to n / 2).filter { next => n % next == 0 }
+
+  def solve = {
+    val sumOfDivisors = (1 to 10000).map { n => (n -> divisors(n).sum) }.toMap
+    val amicables = sumOfDivisors.filterKeys { k =>
+      k != sumOfDivisors(k) && k == sumOfDivisors.getOrElse(sumOfDivisors(k), -1)
+    }
+    amicables.keys.sum
+  }
+
+}
---
+++ src/Euler0022.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+
+object Euler0022 extends EulerSolution {
+
+  override def expect = 871198282
+
+  def solve = {
+    val allNames = Source.fromFile("src/Euler0022.names.txt").getLines().next()
+    val names = allNames.split(",").toList.map(_.replaceAll("\"", "")).sortWith((a, b) => a.compareTo(b) < 0)
+
+    def sumOfScores(remainingNames: List[String], index: Int, sum: Int): Int = remainingNames match {
+      case Nil => sum
+      case _ => {
+        val newSum = sum + remainingNames.head.map(c => c.toInt - 'A'.toInt + 1).sum * index
+        sumOfScores(remainingNames.tail, index + 1, newSum)
+      }
+    }
+
+    sumOfScores(names, 1, 0)
+  }
+
+}
