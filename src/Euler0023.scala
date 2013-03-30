349f544e7cb4716aee57496ea90a5e6257911f2d
Greg Merrill <greghmerrill@gmail.com>
2013-03-29 21:20:41 -0500

    marginally better performance by ending sum calcuations at 28123

--- src/Euler0023.scala
+++ src/Euler0023.scala
@@ -4,6 +4,18 @@ object Euler0023 extends EulerSolution {
 
   def divisors(n: Int) = (1 to n / 2).filter { next => n % next == 0 }
 
+  def mapWhile(xs: List[Int], mapFn: Int => Int, predicate: Int => Boolean) = {
+    def mapWhileIter(xs: List[Int], result: List[Int]): List[Int] = xs match {
+      case Nil => result
+      case _ => {
+        val mapped = mapFn(xs.head)
+        if (predicate(mapped)) mapWhileIter(xs.tail, mapped :: result)
+        else result
+      }
+    } 
+    mapWhileIter(xs, List[Int]())
+  }
+  
   def solve = {
     println("Calculating abundants")
     val abundants = (1 to 28123).filter(n => divisors(n).sum > n).toList
@@ -11,7 +23,7 @@ object Euler0023 extends EulerSolution {
     println("Calculating sums")
     def calcSums(remainingAbundants: List[Int], sums: Set[Int]): Set[Int] = remainingAbundants match {
       case Nil => sums
-      case _ => calcSums(remainingAbundants.tail, sums ++ remainingAbundants.map(n => remainingAbundants.head + n))
+      case _ => calcSums(remainingAbundants.tail, sums ++ mapWhile(remainingAbundants, n => remainingAbundants.head + n, _ <= 28123))
     }
     val unsummable = (1 to 28123).toSet &~ calcSums(abundants, Set())
     unsummable.sum
