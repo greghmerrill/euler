88dd0e0d414dc6b8a75325c4812a7612decb9a1e
Greg Merrill <greghmerrill@gmail.com>
2013-04-04 22:47:45 -0500

    solution 30

---
+++ src/Euler0030.scala
@@ -0,0 +1,15 @@
+import scala.math.pow
+
+object Euler0030 extends EulerSolution {
+
+  override def expect = 443839
+  
+  def solve = {
+    val powers = Map() ++ (0 to 9).map(a => a -> pow(a, 5).toInt)
+    val matches = (2 to pow(9, 6).toInt).filter { x =>
+      x.toString.map(c => powers(c.asDigit)).sum == x
+    }
+    matches.sum
+  }
+
+}
