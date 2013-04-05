546b4107b0bcd01ff12011faf1355f8b512ac2ee
Greg Merrill <greghmerrill@gmail.com>
2013-04-04 20:18:52 -0500

    solution 29

---
+++ src/Euler0029.scala
@@ -0,0 +1,12 @@
+object Euler0029 extends EulerSolution {
+
+  override def expect = 9183
+
+  def solve = {
+    val powers = (2 to 100) flatMap { a =>
+      (2 to 100) map { b => BigInt(a).pow(b) }
+    }
+    (Set() ++ powers).size
+  }
+
+}
