7c17071af867910b9784f4ee744e4ce0e95d7008
Greg Merrill <greghmerrill@gmail.com>
2013-04-06 17:35:42 -0500

    solution 32

---
+++ src/Euler0032.scala
@@ -0,0 +1,48 @@
+object Digits {
+  val list = (1 to 9).toList
+  val set = Set() ++ list
+}
+
+object Euler0032 extends EulerSolution {
+
+  override def expect = 45228
+
+  /** @return all ordered, non-repeating combinations with "size" elements from the given vals */
+  def combos(vals: List[Int], size: Int): List[List[Int]] = {
+    def comboIter(combo: List[Int], remainingVals: List[Int]): List[List[Int]] = {
+      if (combo.size == size) List(combo)
+      else {
+        remainingVals.map(v => comboIter(v +: combo, remainingVals.filterNot(_ == v))).flatten
+      }
+    }
+    comboIter(List(), vals)
+  }
+
+  def toInt(digits: List[Int]): Int = digits.mkString.toInt
+
+  def isPanDigital(a: List[Int], b: List[Int], p: Int) = {
+    val str = toInt(a).toString + toInt(b).toString + p.toString
+    str.length == 9 && Set() ++ str.map(_.asDigit) == Digits.set
+  }
+
+  def panDigitalProducts(multiplicand: List[Int], multipliers: List[List[Int]]): List[Int] = {
+    val products = multipliers map { x => (x, toInt(multiplicand) * toInt(x)) }
+    val panDigitalsOnly = products.filter { mz =>
+      isPanDigital(multiplicand, mz._1, mz._2)
+    }
+    panDigitalsOnly.map { mz => mz._2 }
+  }
+
+  def panDigitalProductsForMultiplicand(multiplicand: List[Int]): List[Int] = {
+    val multipliers = combos(Digits.list.filterNot(multiplicand.contains(_)), 5 - multiplicand.size)
+    panDigitalProducts(multiplicand, multipliers)
+  }
+
+  def solve = {
+    val pandigitalProducts = (1 to 2) map { size =>
+      combos(Digits.list, size) map panDigitalProductsForMultiplicand
+    }
+    (Set() ++ pandigitalProducts.flatten).flatten.sum
+  }
+
+}
