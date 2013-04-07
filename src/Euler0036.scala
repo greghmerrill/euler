b3b7a91b83f4ebd4063d2f4bcb7b0bf953faa16c
Greg Merrill <greghmerrill@gmail.com>
2013-04-07 12:32:46 -0500

    make more succinct

--- src/Euler0036.scala
+++ src/Euler0036.scala
@@ -1,7 +1,7 @@
 object Euler0036 extends EulerSolution {
 
-  def isPalindrome(s: String) = s.equals(s.toString.reverse)
+  def isPal(s: String) = s.equals(s.toString.reverse)
 
-  def solve = 1.to(999999, 2) filter { x => isPalindrome(x.toString) && isPalindrome(Integer.toString(x, 2)) } sum
+  def solve = 1.to(999999, 2) filter { x => isPal(x.toString) && isPal(Integer.toString(x, 2)) } sum
 
 }
